6c612e7d6da6604410dbc9667fe5294170b5f94c
Garima29 <dadheechgarima371@gmail.com>
2021-10-05 14:59:18 +0530

    added login and login feature

---
+++ js/login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginBtn").onclick=function(){
+    const name=document.getElementById("uname").value;
+    const pass=document.getElementById("pwd").value;
+    fetch("/js/users.json")
+    .then(response => response.json())
+    .then(data => {
+        const users= data;
+        var userExists=false;
+        for(var value in users){
+            if((users[value].name == name) && (users[value].password == pass)){
+                userExists = true;
+                break;
+            }
+        }
+        if(userExists){
+            alert("Logged in successfully");
+            localStorage["username"]=name;
+            location.href="home.html";
+        }
+        else{
+            alert("Invalid Credentials");
+        }
+        })
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-// Add JavaScript Code here
+setUser();
+document.getElementById("logout").onclick=function(){
+    localStorage.clear();
+    setUser();
+}
+function setUser(){
+    if(localStorage["username"]!== null && localStorage["username"]!== undefined){
+        document.getElementById("name").innerHTML = "Hi "+localStorage["username"];
+    }else{
+        document.getElementById("name").innerHTML = "";
+    }
+    
+}
